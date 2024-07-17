dd0a9a950d9a5af490901d35f668ec47accf4486
Alex Sullon <alexsullonporras@hotmail.com>
2024-07-16 19:15:08 -0500

    Se agregó lógica JS para grabar una categoría

--- public/assets/js/categoria.js
+++ public/assets/js/categoria.js
@@ -8,7 +8,7 @@
         })
     }
 
-    function procesarDatos(acccion){
+    async function procesarDatos(accion){
         const nombre = document.querySelector('#nombre')
         const estado = document.querySelector('#estado')
 
@@ -17,6 +17,41 @@
             return
         }
 
-        Swal.fire('Procesando', "Los datos se están procesando", 'success')
+        const url = '/api/categorias'
+        Swal.fire({
+            title: "Procesando datos",
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            didOpen: () => {
+                Swal.showLoading()
+            }
+        })
+
+
+        const data = new FormData();
+        data.append("nombre", nombre.value)
+        data.append("estado", estado.value)
+        data.append("accion", accion)
+        try {
+            const respuesta = await fetch(url, {
+                method: "POST",
+                body: data
+            })
+            const resultado = await respuesta.json()
+            if(resultado.estado){
+                Swal.fire({
+                    title: resultado.mensaje,
+                    icon: 'success',
+                    allowOutsideClick: false,
+                    allowEscapeKey: false,
+                    timer: 1000,
+                    showConfirmButton: false
+                }).then((result) => {
+                    window.location = '/admin/categorias'
+                })
+            }
+        } catch (error) {
+            Swal.fire("Error", "Ocurrió un error al intentar procesar los datos")
+        }
     }
 })()
